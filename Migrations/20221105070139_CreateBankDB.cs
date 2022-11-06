309fd5f37efe050ce02b369003392826fe3aa146
ZahraDadashi <113887797+ZahraDadashi@users.noreply.github.com>
2022-11-06 15:32:14 +0330

    second commit

--- Migrations/20221105070139_CreateBankDB.cs
+++ Migrations/20221105070139_CreateBankDB.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -31,7 +30,13 @@ namespace BankAccount.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     changeAmt = table.Column<double>(type: "float", nullable: false),
                     Balance = table.Column<double>(type: "float", nullable: false),
-                    time = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    Day = table.Column<int>(type: "int", nullable: false),
+                    Hour = table.Column<int>(type: "int", nullable: false),
+                    Minute = table.Column<int>(type: "int", nullable: false),
+                    Second = table.Column<int>(type: "int", nullable: false),
                     AccNum = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
